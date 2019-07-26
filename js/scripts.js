4cb3abd8f29bd896fdc49756ba3d560e4cb1add6
Adam Ruf <adam@adamruf.com>
2019-07-26 06:53:34 -0700

    Style and content updates

--- js/scripts.js
+++ js/scripts.js
@@ -45,7 +45,7 @@ $('document').ready(function(){
     }
 
     // var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-    var deadline = 'August 14 2019 23:59:59 GMT-0800';
+    var deadline = 'August 18 2019 23:59:59 GMT-0800';
     initializeClock('clockdiv', deadline);
   });
 
