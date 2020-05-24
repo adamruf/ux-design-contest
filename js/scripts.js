988f953b5c89637c5850c42cc70760790157bf0c
Adam Ruf <adam@adamruf.com>
2020-05-24 12:24:51 -0700

    Updated Charity page

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,41 @@
 // Initialize Bootstrap Tooltips
-  $('document').ready(function () {
-    $('[data-toggle="tooltip"]').tooltip();
+$('document').ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+// Count up to the number donated to charity
+$('document').ready(function () {
+  $('.js-counter').each(function () {
+    var $this = $(this),
+      countTo = $this.attr('data-count');
+
+    $({ countNum: $this.text() }).animate({
+      countNum: countTo
+    },
+      {
+        duration: 3000,
+        easing: 'swing',
+        step: function () {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function () {
+          $this.text(this.countNum);
+          //alert('finished');
+        }
+      });
   });
+});
 
-// Countdown Clock used for highlighting deadline for contest entries
-$('document').ready(function(){
-  function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    // var seconds = Math.floor((t / 1000) % 60);
-    // var minutes = Math.floor((t / 1000 / 60) % 60);
-    // var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-      'total': t,
-      'days': days,
-      // 'hours': hours,
-      // 'minutes': minutes,
-      // 'seconds': seconds
-    };
-  }
-
-  function initializeClock(id, endtime) {
-    var clock = document.getElementById(id);
-    var daysSpan = clock.querySelector('.days');
-    var hoursSpan = clock.querySelector('.hours');
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
-
-    function updateClock() {
-      var t = getTimeRemaining(endtime);
-
-      daysSpan.innerHTML = t.days;
-      // hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-      // minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-      // secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-      if (t.total <= 0) {
-        clearInterval(timeinterval);
-      }
-    }
-
-    updateClock();
-    var timeinterval = setInterval(updateClock, 1000);
-    }
-
-    // var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-    var deadline = 'August 18 2020 23:59:59 GMT-0800';
-    initializeClock('clockdiv', deadline);
-  });
+  // $('document').ready(function () {
+  //   $('.js-count').each(function () {
+  //     $(this).prop('Counter', 0).animate({
+  //       Counter: $(this).text()
+  //     }, {
+  //       duration: 3000,
+  //       easing: 'swing',
+  //       step: function (now) {
+  //         $(this).text(Math.ceil(now));
+  //       }
+  //     });
+  //   });
+  // });
